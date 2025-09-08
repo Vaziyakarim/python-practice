7241d9d834473738ff85a7b628b0c0651d1e51a7
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-08 20:15:19 +0530

    IF condition Ex
    IF condition Ex

---
+++ conition.py
@@ -0,0 +1,22 @@
+#EX 1 
+is_hot=False
+is_cold=False
+if is_hot:
+    print("Its a hot day")
+    print("Drink alot of water")
+elif is_cold:
+     print("wear warm clothes")
+    
+else:
+   
+ print("Its a lovely day")
+ 
+ #EX 2
+price=100000
+is_goodCredit=False
+if is_goodCredit:
+    down_payment=0.1 *price
+   
+else:
+     down_payment=0.2 *price
+print(f'Down payment {down_payment}')
