b86a3ebf6c588dc75b0959a01da9598230e30945
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-08 20:13:58 +0530

    String Method
    String Method

---
+++ stringMethond.py
@@ -0,0 +1,11 @@
+name='Karim Vaziya'
+print(len(name))#no.of charactor count
+print(name.upper())#upper case
+print(name.lower())#flower case
+print(name.title())#full print
+print(name.find('m'))#finding index
+print(name.find('M'))
+print(name.find('Vaziya'))
+print(name.replace('Vaziya','vaziya'))#replacing
+print('Karim' in name)# checking
+print('karim' in name)
