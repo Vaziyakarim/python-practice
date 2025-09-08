f94b397c6e04c3aeb74623f7083f44e918d20aa5
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-08 20:16:01 +0530

    Operator
    Operator

---
+++ arithmetic.py
@@ -0,0 +1,44 @@
+# print(10 + 3)
+# print(10 - 3)
+# print(10 * 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+
+# #assignment
+# x=5
+# x+=4
+# print(x)
+
+# #Precedence
+# x= 10*(2-3)
+# print(x)
+# # () ** * / + -
+
+# #Logical
+# is_high_income=True
+# is_good_credit=False
+# has_criminal_record=False
+# if is_high_income and not has_criminal_record:
+#     print("eligible")
+# elif is_high_income or is_good_credit:
+#     print("yes eligible")
+# and or not
+
+#comparision
+
+# temperatur=30
+# if temperatur>=20:
+#     print("its a hot day")
+# else:
+#     print("its not a hot day")
+# #== != > < >= <=
+
+name="vaziya"
+if len(name) < 3:
+    print("name must be atleast 3 characters")
+elif len(name)>50:
+    print("name can be max of 50 character")
+else:
+    print("beautiful name")
