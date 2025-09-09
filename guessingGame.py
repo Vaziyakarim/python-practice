98af1b5397d9540184e2ed004b484005d70ada05
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-09 21:58:24 +0530

    Guessing Number
    Guessing Number

---
+++ guessingGame.py
@@ -0,0 +1,13 @@
+secret_code=9
+guess_count=0
+guess_limit=3
+while guess_count < guess_limit:
+    guess=int(input("guess: "))
+    guess_count +=1
+    if guess==secret_code:
+         print("you Win")
+         break
+    
+       
+else:
+     print("Sorry Better Luck Next Time")
