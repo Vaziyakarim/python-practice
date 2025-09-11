fbb5395f934efb9a832b923c585cad76951f696b
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-11 21:38:49 +0530

    Nested loop example
    Nested loop example

---
+++ NestedLoopEX.py
@@ -0,0 +1,14 @@
+#Ex-1
+# for i in range(3):
+  for j in range(3):
+     print(f'({i},{j})')
+#Ex-2
+x=[5,2,5,2,2]
+for i in x:
+    for j in range(i):
+    
+     print('x',end='')
+    print() 
+#Ex-3
+for item in ['vaziya','karim','shaik']:
+    print(item)
