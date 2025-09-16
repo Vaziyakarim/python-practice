6b8e02d1bf12c4fc8df169b1a637d45a342f86ba
vaziya karim <karimvaziya545@gmail.com>
2025-09-16 22:53:24 +0530

    Random value ex
    Random value ex

---
+++ randomValue.py
@@ -0,0 +1,22 @@
+import random
+# for i in range(3):
+#  print(random.random())#Random in points
+ 
+#  print(random.randint(10,20))#btw particular number
+ 
+#  #EX-2:
+# members=['vaziya','apsa','karim']
+# leader=random.choice(members)
+# print(leader)
+
+# #EX:3
+# class Dice:
+#     def roll(self):
+#        first= random.randint(1,6)
+#        second=random.randint(1,6)
+#        return first,second
+        
+
+# dice=Dice()
+# print(dice.roll())       
+        
